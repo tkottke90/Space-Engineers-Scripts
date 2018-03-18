45ea5748934f48f462fb8915bb79c2f47831603c
tkottke90 <altotom90@gmail.com>
2018-03-18 16:38:59 -0500

    Built Interior Light Offeset Script

---
+++ InteriorLightOffsetScript/Program.cs
@@ -0,0 +1,98 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        // =======================================================================================
+        //                                                                            --- Configuration ---
+        // =======================================================================================
+
+        // --- Script Tag ---
+        // =======================================================================================
+        // Add tag to Interior Light Blocks you wish for this script to control.
+        // Example: Interior Light 1 [AVLIGHT]
+        string SCRIPT_TAG = "AVLIGHT";
+
+        // =======================================================================================
+
+        // --- Light Offset ---
+        // =======================================================================================
+        // Enter the offset value as a number plus 'f' (sets the number as a float)
+        // Example: float OFFSET = 5f;
+        float OFFSET = 5f;
+
+        // =======================================================================================
+
+        // DO NOT EDIT BELOW THIS LINE!!
+        // =======================================================================================
+
+
+        string tag_pattern;
+        System.Text.RegularExpressions.Regex tag_match;
+
+        public Program()
+
+        {
+            Runtime.UpdateFrequency = UpdateFrequency.Once;
+            tag_pattern = @"(\[" + SCRIPT_TAG + @")(\s\b[a-zA-z-]*\b)*(\s?\])";
+            tag_match = new System.Text.RegularExpressions.Regex(tag_pattern);
+
+        }
+
+        public void Main()
+
+        {
+            List<IMyInteriorLight> intLights = new List<IMyInteriorLight>();
+            GridTerminalSystem.GetBlocksOfType<IMyInteriorLight>(intLights, (IMyInteriorLight i) => tag_match.IsMatch(i.CustomName));
+
+            if (intLights.Count > 0)
+            {
+                ITerminalProperty<float> x = intLights[0].GetProperty("Offset").AsFloat();
+
+                foreach (IMyInteriorLight light in intLights)
+                {
+                    x.SetValue(light, OFFSET);
+                }
+
+                Echo(DrawApp());
+            }
+            else
+            {
+                Echo(DrawApp("No Lights Found!!\nPlease Tag Managed Lights and Click Run"));
+            }
+
+        }
+
+        public string DrawApp(string message = "")
+        {
+            StringBuilder output = new StringBuilder();
+
+            output.AppendLine("-- NK Light Update Script --").AppendLine();
+
+            if (message.Length == 0)
+            {
+                output.AppendLine("Run Successful - Lights Updated");
+            }
+            else
+            {
+                output.AppendLine(message);
+            }
+            return output.ToString();
+        }
+    }
+}
---
+++ InteriorLightOffsetScript/Readme.cs
@@ -0,0 +1,15 @@
+﻿// *** N00bKeper Light Offset Update Script ***
+// =======================================================================================
+
+// Script automatically updates the Offset value to a predefined value.The script uses
+// the UpdateFrequency property available in the Runtime of the Programmable Block to run
+// the script once when the world loads.
+
+// =======================================================================================
+//                                                                            --- Setup ---
+// =======================================================================================
+
+//	1) Add selected tag to the Interior Lights you wish to have automatically controlled
+//  2) Run Script
+//  
+//  3) When world loads or script is recompiled, the script will automatilcally run once.
